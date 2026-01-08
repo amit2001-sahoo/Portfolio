72652a003f4733d934b360f80993eb35293f1f0c
Amit Kumar Sahoo <CZ0223@cztech.com>
2026-01-08 16:16:41 +0530

    added api upgradation

--- api/contact.js
+++ api/contact.js
@@ -9,18 +9,6 @@ export default async function handler(req, res) {
     }
     const {name, email, message} = req.body
 
-    if(!name){
-        return res.status(400).json({message:"missing field 'name'"})
-    }
-
-    if(!email){
-        return res.status(400).json({message:"missing field 'email'"})
-    }
-
-    if(!message){
-        return res.status(400).json({message:"missing field 'message'"})
-    }
-
     try{
         const transporter = nodemailer.createTransport({
             service: "gmail",
