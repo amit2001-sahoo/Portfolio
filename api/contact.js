42abb2cdb479f279c4bf8f7f265a6b704e54c509
Amit Kumar Sahoo <CZ0223@cztech.com>
2026-01-08 16:19:34 +0530

    added api upgradation

--- api/contact.js
+++ api/contact.js
@@ -1,8 +1,6 @@
 import nodemailer from "nodemailer"
 
 export default async function handler(req, res) {
-    const EMAIL_USER = "sipunamit474@gmail.com"
-    const EMAIL_PASS = "Amit@2001"
     console.log("inside the mailer function")
     if (req.method != "POST") {
         return res.status(405).json({message:"method not allowed"})
@@ -13,13 +11,13 @@ export default async function handler(req, res) {
         const transporter = nodemailer.createTransport({
             service: "gmail",
             auth: {
-                user: EMAIL_USER,
-                pass: EMAIL_PASS,
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
             }
         })
         await transporter.sendMail({
             from: email,
-            to: EMAIL_USER,
+            to: process.env.EMAIL_USER,
             subject: `New Message From ${name}`,
             text: `
             
